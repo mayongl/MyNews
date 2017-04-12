cec007bf646a6e9edeb807d985597feaba2bfe1a
Yonglin <mayongl@apache.org>
2017-04-12 22:04:09 +0800

    Initial Commit.

---
+++ MyNews/MyNews/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyNews
+//
+//  Created by Yonglin Ma on 4/12/17.
+//  Copyright © 2017 Sixlivesleft. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
