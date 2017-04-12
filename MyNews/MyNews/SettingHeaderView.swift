22011cb67dbfdf802ab3a4e930f152c2b227b624
Yonglin <mayongl@apache.org>
2017-04-13 07:15:59 +0800

    Page for setting. not completed.

---
+++ MyNews/MyNews/SettingHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingHeaderView.swift
+//  MyNews
+//
+//  Created by Yonglin Ma on 4/12/17.
+//  Copyright © 2017 Sixlivesleft. All rights reserved.
+//
+
+import UIKit
+
+class SettingHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
